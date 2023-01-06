205aa857dbfb1bb87c779fe4f45cd4c0339122de
Peter Duffy <ptrduffy@outlook.com>
2023-01-06 11:40:48 +0000

    Start of Ch19

---
+++ src/Ch19.purs
@@ -0,0 +1,11 @@
+module Ch19 where
+
+import Prelude
+
+import Effect (Effect)
+import Effect.Console (log)
+
+test :: Effect Unit
+test = do
+  log "placeholder"
+
---
+++ src/Main.purs
@@ -0,0 +1,8 @@
+module Main where
+
+import Prelude
+import Effect (Effect)
+import Ch19 as Ch19
+
+main :: Effect Unit
+main = Ch19.test
